7051b6a4c8b37625724dcc82a4208544755b88a1
Sanket Botre <sanketbotre24@gmail.com>
2026-08-03 23:31:00 +0530

    Initial commit: project setup

---
+++ QueryOpsCloud/src/main/java/me/codewithsanket/QueryOpsCloud/QueryOpsCloudApplication.java
@@ -0,0 +1,13 @@
+package me.codewithsanket.QueryOpsCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryOpsCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryOpsCloudApplication.class, args);
+	}
+
+}
---
+++ QueryOpsCloud/src/test/java/me/codewithsanket/QueryOpsCloud/QueryOpsCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package me.codewithsanket.QueryOpsCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryOpsCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
